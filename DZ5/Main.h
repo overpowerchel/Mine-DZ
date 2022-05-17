0a3b91b5ea6c78816e27295519c30288340432e5
overpowerchel <102666320+overpowerchel@users.noreply.github.com>
2022-05-18 01:23:05 +0300

    Add files via upload

---
+++ DZ5/Main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <iostream>
+
+
+using namespace std;
+
+bool InCheck(int num);
+void FillArr(int** arr, int n, int m);
+void OutArr(int** arr, int n, int m);
+
+#endif 
---
+++ DZ5/main.cpp
@@ -0,0 +1,57 @@
+﻿#include "Main.h"
+
+int main()
+{
+	srand(time(0));
+	int n;
+	int m;
+	cout << "Enter Array height:" << endl;
+	cin >> n;
+	if (!InCheck(n)) return 0;
+	cout << "Enter Array width:" << endl;
+	cin >> m;
+	if (!InCheck(m)) return 0;
+	int** Arr = new int* [n];
+	for (int i = 0; i < n; i++) Arr[i] = new int[m];
+	FillArr(Arr, n, m);
+	OutArr(Arr, n, m);
+	for (int i = 0; i < n; i++) delete[] Arr[i];
+	delete[] Arr;
+}
+
+
+void FillArr(int** Arr, int n, int m)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			Arr[i][j] = rand() % 41 + 10;
+		}
+	}
+}
+
+void OutArr(int** Arr, int n, int m)
+{
+	cout << "\n";
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+
+		{
+			cout << Arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+bool InCheck(int num)
+{
+	if (!(cin) || num < 1)
+	{
+		cout << "Input error";
+		return false;
+	}
+	else return true;
+}
+
